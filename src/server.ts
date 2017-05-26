6cd38e34f8d575d3d952655dd19d766ea453284c
foisonocean <foisonocean@gmail.com>
2017-05-26 13:29:46 +0800

    Add mongoDB for database

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,34 @@ import * as express from 'express'
 import * as cookieParser from 'cookie-parser'
 import * as bodyParser from 'body-parser'
 
+import * as mongoose from 'mongoose'
+
+mongoose.connect('mongodb://localhost/gqldemo')
+
+const peopleSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  age: Number,
+  phone: String,
+  hobby: String,
+  lover: Number,
+})
+const People = mongoose.model('People', peopleSchema, 'people')
+
 const server = express()
 
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: false }))
 server.use(cookieParser())
 
+server.get('/people', (req, res, next) => {
+  People.find((err, docs) => {
+    res.send(docs)
+  })
+})
+
 server.get('/', (req, res, next) => {
-  res.send('hello, TypeScript™ based express')
+  res.send(JSON.stringify('hello'))
 })
 
 server.listen(3000, '0.0.0.0', () => {
